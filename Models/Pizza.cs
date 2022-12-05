89ae7e620dffbac50be269f9f4722404fe2ac1d5
CRISTIAN MUSAT <cmg.musat@gmail.com>
2022-12-05 14:49:32 +0100

    Closed #1

--- Models/Pizza.cs
+++ Models/Pizza.cs
@@ -1,13 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace ContosoPizza.Models
 {
     public class Pizza
     {
         public int Id { get; set; }
+        [Required]
         public string? Name { get; set; }
         public bool IsGlutenFree { get; set; }
     }
